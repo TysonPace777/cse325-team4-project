209ebb18f57cfd61eb66b2de589dc3fdf75625df
tysonpace777 <32.tpace@gmail.com>
2026-05-13 12:55:08 -0600

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cse325_team4_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
